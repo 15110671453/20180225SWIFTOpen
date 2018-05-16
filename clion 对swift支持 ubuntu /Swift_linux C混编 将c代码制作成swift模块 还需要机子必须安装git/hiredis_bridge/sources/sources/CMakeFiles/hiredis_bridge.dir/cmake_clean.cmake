0ca7b21110256b03e11951669c8c939e6e382036
15110671453 <15110671453@163.com>
2018-05-16 18:52:53 +0800

    使用cmake 将要混编的c 提前制作为静态库
    使用cmake 将要混编的c 提前制作为静态库

---
+++ clion 对swift支持 ubuntu /Swift_linux C混编 将c代码制作成swift模块 还需要机子必须安装git/hiredis_bridge/sources/sources/CMakeFiles/hiredis_bridge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hiredis_bridge.dir/hiredis_bridge.c.o"
+  "../libdir/libhiredis_bridge.pdb"
+  "../libdir/libhiredis_bridge.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/hiredis_bridge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ clion 对swift支持 ubuntu /Swift_linux C混编 将c代码制作成swift模块 还需要机子必须安装git/hiredis_bridge/sources/sources/CMakeFiles/hiredis_bridge.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "../libdir/libhiredis_bridge.a"
+)
